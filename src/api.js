75c6ddee792603bf2e9d168d7c0765191af10e7a
EmilioJaramillo <em.jaramillo@duocuc.cl>
2025-12-02 11:33:04 -0300

    se agrega la ip conexion
    se agrega la ip de conexión al servidor bd

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://100.28.221.4:8080/api";
 
 // ----- TOKEN -----
 
